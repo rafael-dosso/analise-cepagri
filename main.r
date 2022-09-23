441dec8b3f26d6de585ff7973fd84f22bd5895f7
Rafael Dosso <rafael.dosso@gmail.com>
2022-09-23 09:00:10 -0300

    Filtro anos, temp e umid

--- main.r
+++ main.r
@@ -2,11 +2,22 @@ setwd('/Users/u20154/Documents/analise-cepagri')
 names <- c("horario", "temp", "vento", "umid", "sensa")
 cepagri <- read.csv("./cepagri.csv", header = FALSE, sep = ";", col.names = names)
 
-# converte os valores para numericos
-cepagri <- as.numeric(cepagri)
-# aqueles que estavam em string, como "[ERRO]"", ficam como NA, e logo sao
-# retirados da tabela
-cepagri <- cepagri[!(is.na(cepagri$horario) | is.na(cepagri$temp) |
-is.na(cepagri$vento) | is.na(cepagri$umid) | is.na(cepagri$sensa)), ]
+# converte os valores de temperatura para valores numericos
+cepagri$temp <- as.numeric(cepagri$temp)
 
-# ainda nao esta funcionando
+cepagri$horario <- as.POSIXlt(cepagri$horario, format="%d/%m/%Y-%H:%M")
+
+# retira as linhas antes de 2015 ou depois de 2021
+cepagri <- cepagri[(cepagri$horario$year + 1900 > 2014 &
+    cepagri$horario$year + 1900 < 2022),]
+
+# retira linhas com temperaturas e umidades improvaveis
+cepagri <- cepagri[cepagri$temp > -10.0 & cepagri$temp < 45.0,]    
+cepagri <- cepagri[(cepagri$umid != 100.0 & cepagri$umid != 0),]    
+
+# retira as linhas que contém erros, ou seja, são NA
+# cepagri <- cepagri[!(is.na(cepagri$horario) | is.na(cepagri$temp) |
+#     is.na(cepagri$vento) | is.na(cepagri$umid) | is.na(cepagri$sensa)), ]
+
+# summary(cepagri)
+tail(cepagri, 5)
